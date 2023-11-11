dec49e98147ca53cf9dfbf142134403b32eaf3b4
mangwoncassie <dudnfsla1@naver.com>
2023-11-11 11:59:44 +0900

    result is not yet done just console.log

--- src/components/NewPayment/PaymentForm.js
+++ src/components/NewPayment/PaymentForm.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './PaymentForm.css';
 
 const PaymentForm = () => {
+
+  const [objectState, setObjectState] = useState({
+    name: '',
+    price: 0,
+    today: new Date(),
+  })
+
+
+  const inputTextHandler = (event) => {
+    setObjectState(prevState => ({...prevState, name:event.target.value}))
+  }
+
+  const inputPriceHandler  = (event) => {
+    setObjectState(prevState => ({...prevState, price:event.target.value}))
+  }
+
+  const inputTodayHandler  = (event) => {
+      setObjectState(prevState => ({...prevState, today:event.target.value}))
+  }
+
+  const buttonEventHandler = (event) => {
+    event.preventDefault();
+    /*event가 매개변수로 */
+    console.log(objectState);
+
+    /*초기값 복사해서 넣어주면 초기화됨 */
+    setObjectState({
+      name: '',
+      price: 0,
+      today: new Date(),
+    });
+  };
+
+ /*테스트 해보면 */
+  console.log(objectState); 
   return (
     <form>
       <div className='new-payment__controls'>
         <div className='new-payment__control'>
           <label>이름</label>
-          <input type='text' />
+          <input type='text'  onChange={inputTextHandler} value={objectState.name}/>
         </div>
         <div className='new-payment__control'>
           <label>금액</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01'   onChange={inputPriceHandler}
+            value={objectState.price}
+          />
         </div>
         <div className='new-payment__control'>
           <label>날짜</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input type='date' min='2019-01-01' max='2023-12-31'  onChange={inputTodayHandler}
+          value={objectState.today} />
         </div>
       </div>
       <div className='new-payment__actions'>
