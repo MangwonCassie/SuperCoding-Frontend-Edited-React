8394ca2a49bd5a8f7524cf976b70269d54d1011e
mangwoncassie <dudnfsla1@naver.com>
2023-11-18 19:54:21 +0900

    lecture 71 is finished

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
+  const counter = useSelector((state)=>state.counter);
+  const dispatch = useDispatch();
+
+
+  const incrementHandler = () => {
+    dispatch({type: "increment"});
+  }
+
+  const decrementHandler = () => {
+    dispatch({type: "decrement"});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- 카운터 값 --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>토글 카운터</button>
+      <button onClick={incrementHandler}>증감 카운터</button>
+      <button onClick={decrementHandler}>감소 카운터</button>
     </main>
   );
 };
