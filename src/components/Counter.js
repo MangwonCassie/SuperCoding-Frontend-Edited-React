15e8abe1faa65f694299482d812f21fa8fc8171d
mangwoncassie <dudnfsla1@naver.com>
2023-11-19 14:01:50 +0900

    lecture 72 is done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: "toggle"})
+  };
   const counter = useSelector((state)=>state.counter);
+  const showCounter = useSelector((state)=>state.showCounter);
   const dispatch = useDispatch();
 
 
@@ -15,12 +18,17 @@ const Counter = () => {
     dispatch({type: "decrement"});
   }
 
+  const increase10Handler = () => {
+    dispatch({type: "increase", amount: 10});
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>증감 카운터</button>
+      <button onClick={increase10Handler}>숫자 10 증가 카운터</button>
       <button onClick={decrementHandler}>감소 카운터</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,39 @@
 import {createStore} from 'redux'
 
-const counterReducer = (state = {counter : 0}, action) => {
+const counterReducer = (state = {counter : 0, showCounter: true}, action) => {
     if(action.type === 'increment'){
         return {
-            counter: state.counter + 1
+            ...state,
+            counter: state.counter + 1,
         }
     }
 
     if(action.type === 'decrement'){
         return {
-            counter: state.counter - 1
+            ...state,
+            counter: state.counter - 1,
+        }
+    }
+
+
+
+    if(action.type === 'increase'){
+        return {
+            ...state,
+            counter: state.counter + action.amount,
+        }
+    }
+
+    if(action.type === 'toggle'){
+        return {
+            ...state,
+            showCounter: !state.showCounter,
         }
     }
     return state;
+
+
+    
 }
 
 const store = createStore(counterReducer);
